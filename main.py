ae50240cc3369e52103315f07f6ba743bf037646
yanezreed <158992507+yanezreed@users.noreply.github.com>
2026-09-22 19:46:23 +0100

    main.py
    Added a basic display window using PySide6.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from PySide6.QtWidgets import
+import sys
 
+class display_window(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Display Application")
+        self.resize()
+
+        # project will showcase self made animation...
+
+app = QApplication(sys.argv)
+
+app.setStyleSheet(style_sheet)
+
+window = MainWindow()
+window.show()
+
+sys.exit(app.exec())
